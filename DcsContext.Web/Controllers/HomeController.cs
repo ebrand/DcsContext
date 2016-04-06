3a482a3bbdccb947b5c576a324832eb7c7d11ee5
Eric Brand <eric.d.brand@gmail.com>
2016-04-06 16:40:28 -0500

    Initial commit

---
+++ DcsContext.Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace DcsContext.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+	    public IActionResult Voronoi()
+	    {
+		    return View();
+	    }
+    }
+}
